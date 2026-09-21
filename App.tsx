8c58f7d4780b9c5d2648a1152a2fc61152ca9a6b
HugueAX <rvnchsn@gmail.com>
2026-09-20 23:15:34 -0400

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+
+function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>
+          My favorite color is Green!
+        </Alert>
+      )}
+      <Button color="secondary" onClick={() => setAlertVisibility(true)}>
+        Click to find out my favorite color
+      </Button>
+    </div>
+  );
+}
+
+export default App;
